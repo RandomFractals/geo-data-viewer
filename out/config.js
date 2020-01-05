e8ea565a4dcac25438c32f638f11ed20b25308b3
Taras Novak <taras.novak@randomfractals.com>
2020-01-05 10:05:21 -0600

    #7 activate geo data viewer for .json map config files for now
    to enable kepler.gl map view from map config .json gist files

--- out/config.js
+++ out/config.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const logger_1 = require("./logger");
 // log level setting for prod. vs. dev run of this ext.
 exports.logLevel = logger_1.LogLevel.Info; // change to .Debug for ext. dev debug
-exports.supportedDataFiles = /.*\.(csv|geojson)/;
+exports.supportedDataFiles = /.*\.(csv|json|geojson)/;
 //# sourceMappingURL=config.js.map
--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,4 @@ import { LogLevel } from "./logger";
 // log level setting for prod. vs. dev run of this ext.
 export const logLevel: LogLevel = LogLevel.Info; // change to .Debug for ext. dev debug
 
-export const supportedDataFiles: RegExp = /.*\.(csv|geojson)/;
+export const supportedDataFiles: RegExp = /.*\.(csv|json|geojson)/;
