95844b1bc59aa82c8c012a2a9d8e602db70a81a6
Taras Novak <taras.novak@randomfractals.com>
2020-01-09 11:50:34 -0600

    #24 removed /out folder from repo

--- out/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const logger_1 = require("./logger");
-// log level setting for prod. vs. dev run of this ext.
-exports.logLevel = logger_1.LogLevel.Info; // change to .Debug for ext. dev debug
-exports.supportedDataFiles = /.*\.(csv|json|geojson)/;
-exports.mapboxToken = 'pk.eyJ1IjoiZGF0YXBpeHkiLCJhIjoiY2s1Mm10bHB1MThnbDNrdGVmemptd3J5eSJ9.xewq9dOWQLemerED1-qPXQ';
-//# sourceMappingURL=config.js.map
