cfc5058f2003509b87146282e1fd81c28f5158fc
Taras Novak <taras.novak@randomfractals.com>
2020-01-12 11:15:03 -0600

    #23 added json, csv, html export save file filters to config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,3 +6,9 @@ export const logLevel: LogLevel = LogLevel.Info; // change to .Debug for ext. de
 export const supportedDataFiles: RegExp = /.*\.(csv|json|geojson)/;
 
 export const mapboxToken: string = 'pk.eyJ1IjoiZGF0YXBpeHkiLCJhIjoiY2s1Mm10bHB1MThnbDNrdGVmemptd3J5eSJ9.xewq9dOWQLemerED1-qPXQ';
+
+export const saveFileFilters: any = {
+  'JSON': ['json', 'geojson', 'kgl.json'],
+  'CSV': ['csv'],
+  'Html': ['html']
+};
