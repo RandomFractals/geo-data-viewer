eb63e57daf5161f7ddaa7289e37436a37804804e
Taras Novak <taras.novak@randomfractals.com>
2020-01-12 14:09:00 -0600

    #26 added custom kgl files mapping for keplerGL map configs
    per convo with https://github.com/heshan0131 in keplerGL slack

--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,14 @@ import { LogLevel } from "./logger";
 // log level setting for prod. vs. dev run of this ext.
 export const logLevel: LogLevel = LogLevel.Info; // change to .Debug for ext. dev debug
 
-export const supportedDataFiles: RegExp = /.*\.(csv|json|geojson)/;
+export const supportedDataFiles: RegExp = /.*\.(csv|json|geojson|kgl)/;
 
 export const mapboxToken: string = 'pk.eyJ1IjoiZGF0YXBpeHkiLCJhIjoiY2s1Mm10bHB1MThnbDNrdGVmemptd3J5eSJ9.xewq9dOWQLemerED1-qPXQ';
+
+export const openFileFilters: any = {
+  'JSON': ['json', 'geojson', 'kgl.json'],
+  'CSV': ['csv']
+};
 
 export const saveFileFilters: any = {
   'JSON': ['json', 'geojson', 'kgl.json'],
